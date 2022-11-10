4cac289cec9ef52b1bf8da40443d576e21d56f7d
Zen <muhzaini30@gmail.com>
2022-11-10 11:10:50 +0800

    Oke

---
+++ index.mjs
@@ -0,0 +1,20 @@
+import Swal from "sweetalert2";
+
+export default function (teks) {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 1000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+
+  Toast.fire({
+    icon: "success",
+    title: teks,
+  });
+}
